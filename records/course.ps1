05a6a42deaf768bcf6cdfae3e1a18c07202a781c
Desktop <iPhone4s2008@iCloud.com>
2023-04-15 00:27:58 +0800

    update

--- records/course.ps1
+++ records/course.ps1
@@ -1,5 +1,5 @@
-if (!(docker info)) {
-    Write-Error "You need to start Docker first."
+if (!(docker info | Out-Null )) {
+    Write-Output "You need to start Docker first."
     Exit 1
 } else {
     docker run -it --rm -v "${PWD}:/app/records" -v "${PWD}:/root/Downloads" undefined443/course:amd64 index.js $args[0] $args[1] $args[2] $args[3]
